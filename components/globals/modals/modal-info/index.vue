0d24816c031ba5349b63e90d0f68f8dc901b7bfe
vivianeglz <viviane.gonzalez@outlook.fr>
2022-05-29 15:34:58 +0200

    :sparkles: (globals) : Add modal info component

---
+++ components/globals/modals/modal-info/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="modal">
+    <div class="modal__container" :class="className">
+      <div class="modal__header">
+        <h3 v-if="title">{{ title }}</h3>
+        <button class="modal__btn-close" @click.prevent="$emit('close')">
+          <icon-close />
+        </button>
+      </div>
+      <slot />
+      <div v-if="$slots.footer" class="modal__footer">
+        <slot name="footer" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { IconClose } from "@/components/globals/icons";
+
+export default {
+  name: "ModalInfo",
+  components: {
+    IconClose
+  },
+  props: {
+    title: {
+      type: String,
+      default: "Title"
+    },
+    size: {
+      type: String,
+      default: "s",
+      validator(value) {
+        return ["s", "m", "l"].includes(value);
+      }
+    }
+  },
+  computed: {
+    className() {
+      return `modal__container--${this.size}`;
+    }
+  }
+};
+</script>
